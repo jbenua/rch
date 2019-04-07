6548a39abc7b48db487b8ca96f43e77f4ec00cc7
Eugenia Soldatenkova <e.soldatenkova@corp.mail.ru>
2019-04-07 15:15:27 +0300

    task 1

---
+++ task1/nest.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+from itertools import groupby
+
+
+def group_by_key(items, nest_keys):
+    return group_items(items, nest_keys, nest_keys)
+
+
+def group_items(items, nest_keys, rest_keys):
+    if not rest_keys:
+        return items
+    res = {}
+    current_key, *rest = rest_keys
+    for (key, values) in groupby(items, lambda x: x.get(current_key)):
+        if not key:
+            raise KeyError("`%s` is missing in some items" % current_key)
+        if rest:
+            res[key] = group_items(values, nest_keys, rest)
+        else:
+            res[key] = [{k: v[k] for k in v if k not in nest_keys}
+                        for v in values]
+    return res
+
+
+if __name__ == "__main__":
+    nest_keys = sys.argv[1:]
+    items = json.loads(sys.stdin.read())
+    sys.stdout.write(json.dumps(group_by_key(items, nest_keys)))
---
+++ task1/tests.py
@@ -0,0 +1,102 @@
+import random
+import unittest
+
+from copy import deepcopy
+
+from nest import group_by_key
+
+
+ITEMS = [
+    {
+        "country": "US",
+        "city": "Boston",
+        "currency": "USD",
+        "amount": 100
+    },
+    {
+        "country": "FR",
+        "city": "Paris",
+        "currency": "EUR",
+        "amount": 20
+    },
+    {
+        "country": "FR",
+        "city": "Lyon",
+        "currency": "EUR",
+        "amount": 11.4
+    }
+]
+
+
+def pop_random_keys(items):
+    safe_items = deepcopy(items)
+    for item in safe_items:
+        item.pop(random.choice(list(item.keys())))
+    return safe_items
+
+
+class TestNest(unittest.TestCase):
+
+    def test_empty_nesting_keys(self):
+        """Return data as is when passed empty array of nesting keys"""
+        self.assertEqual(
+            ITEMS, group_by_key(ITEMS, []),
+            msg="Somehow items changed"
+        )
+
+    def test_missing_keys(self):
+        """Fails whe passed the unknown key"""
+        with self.assertRaises(KeyError):
+            group_by_key(
+                ITEMS,
+                ["currency", "language"]
+            )
+
+    def test_quantum_keys(self):
+        """
+        Fails, when passed an array of items without constant set of keys:
+        some keys will be missing
+        """
+        items = pop_random_keys(ITEMS)
+        with self.assertRaises(KeyError):
+            group_by_key(
+                items,
+                ["currency", "country", "city"]
+            )
+
+    def test_duplicated_keys(self):
+        """
+        Works ok when passed duplicated nested keys:
+        after first appearance one can still group items by this key
+        (strange case, but why not?)
+        """
+
+        self.assertEqual({
+            "USD": {
+                "US": {
+                    "USD": [{
+                        "city": "Boston",
+                        "amount": 100
+                    }]
+                }
+            },
+            "EUR": {
+                "FR": {
+                    "EUR": [{
+                        "city": "Paris",
+                        "amount": 20
+                    }, {
+                        "city": "Lyon",
+                        "amount": 11.4
+                    }]
+                }
+            }
+        }, group_by_key(
+            ITEMS,
+            ["currency", "country", "currency"]
+        ), msg="Failed to group by the same key twice"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
